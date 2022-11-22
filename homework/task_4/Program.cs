bf0cbad766a3531be5616d4c4e6e71b41577971d
BuildTools <unconfigured@null.spigotmc.org>
2022-11-22 21:22:53 +0300

    task_4

---
+++ homework/task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i=1; i<=n; i++)
+{
+    if(i%2==0)
+    Console.WriteLine("{0}",i);
+}
+    
+     
+   
+
