c5ac5ddacb721b93a9d61bb3c43982246c29c1df
BuildTools <unconfigured@null.spigotmc.org>
2022-11-22 17:57:56 +0300

    task_1 error

--- homework/task_2/Program.cs
+++ homework/task_2/Program.cs
@@ -3,17 +3,9 @@ Console.Write("Введите 1-е число");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-е число");
 int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3-е число");
+int c = Convert.ToInt32(Console.ReadLine());
 if(n>m)
-   { 
-      Console.Write("Максимальное значение= ");
-      Console.WriteLine(n);
-   }
+   Console.WriteLine(n);
 else
-   {
-      Console.Write("Максимальное значение= ");
-      Console.WriteLine(m); 
-   }
-
-   
-
-
+    Console.WriteLine(m);   
