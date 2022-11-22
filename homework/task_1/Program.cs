b06f93cb4ca77b0c3852f888e44f4925f9b6b5bd
BuildTools <unconfigured@null.spigotmc.org>
2022-11-22 17:25:26 +0300

    task_1

---
+++ homework/task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-е число");
+int m = Convert.ToInt32(Console.ReadLine());
+if(n>m)
+   Console.WriteLine(n);
+else
+    Console.WriteLine(m);   
+
+
