9729be3c78b3b95424f583d494b96ecb4aaa695c
BuildTools <unconfigured@null.spigotmc.org>
2022-11-22 18:39:50 +0300

    task_3

---
+++ homework/task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число");
+int n = Convert.ToInt32(Console.ReadLine());
+if((n % 2)==0)
+   { 
+      Console.WriteLine("число четное");
+   }
+else
+   {
+      Console.WriteLine("число нечетное");
+   }
