9cc498865127092e7d1e5d5886ef6fd190e83873
BuildTools <unconfigured@null.spigotmc.org>
2022-11-24 21:10:28 +0300

    task_6

---
+++ homework/task_6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("Введите 1-е число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+while(n>=1000)
+{   
+    n=n/10;   
+}
+
+Console.Write(n%10);
+
+
+if (n<100); Console.Write("Нет третьего числа");
+
+
+
+
+
