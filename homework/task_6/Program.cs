fe5d0472f81861e44716ac71c23f50688b45b8b1
BuildTools <unconfigured@null.spigotmc.org>
2022-11-25 19:53:44 +0300

    task_7

--- homework/task_6/Program.cs
+++ homework/task_6/Program.cs
@@ -3,14 +3,14 @@ Console.Write("Введите 1-е число");
 int n = Convert.ToInt32(Console.ReadLine());
 
 while(n>=1000)
-{   
-    n=n/10;   
-}
+{
+    n=n/10;    
+}  
 
-Console.Write(n%10);
+Console.Write(n%10); 
 
+if  (n<100) Console.Write("Нет третьего числа");
 
-if (n<100); Console.Write("Нет третьего числа");
 
 
 
---
+++ homework/task_7/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n>=1 && n<6)
+{
+    Console.Write("нет"); 
+}
+
+else if(n>0 && n<=7)
+{
+
+    Console.WriteLine("да");
+}
