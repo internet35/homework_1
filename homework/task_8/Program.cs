69d0880cae935d973286d7ea76bc4f263fe01902
BuildTools <unconfigured@null.spigotmc.org>
2022-11-29 16:21:23 +0300

    task_8-1

--- homework/task_8/Program.cs
+++ homework/task_8/Program.cs
@@ -1,8 +1,25 @@
 ﻿Console.Clear();
 Console.Write("Введите пятизначное число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-for (int i=10000; i<=n; i++)
-{
-    if(i%10==0)
-    Console.WriteLine("да");
-}
+int n = Convert.ToInt32((Console.ReadLine()));
+int i, remainder = 0, newnum = 0;
+for (i = n; i > 0; i = (i / 10))
+        {
+            remainder = i % 10;
+            Console.Write(remainder);
+
+            newnum = (newnum*10) + remainder;
+
+        }
+
+
+        if (newnum == n)
+        {
+            Console.WriteLine("число-палиндром");
+        }
+        else
+        {
+            Console.WriteLine("число не является палиндромом");
+        }
+
+
+
