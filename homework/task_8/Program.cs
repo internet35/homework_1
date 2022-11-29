8658e99a82ea68e4b33348bafac4d04298d51c8a
BuildTools <unconfigured@null.spigotmc.org>
2022-11-29 13:41:19 +0300

    task_dop

---
+++ homework/task_8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i=10000; i<=n; i++)
+{
+    if(i%10==0)
+    Console.WriteLine("да");
+}
