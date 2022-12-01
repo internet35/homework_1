c37d4be87896f66f396aae47ad759bfa2be2b4bc
BuildTools <unconfigured@null.spigotmc.org>
2022-12-01 14:19:15 +0300

    task_11

---
+++ homework/task_11/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int n = Convert.ToInt32((Console.ReadLine()));
+Console.Write("Введите второе число: ");
+int m = Convert.ToInt32((Console.ReadLine()));
+int i; 
+int result =1;
+
+for (i = 1; i <= m; i ++)
+{
+    result = result*m;
+
+}
+ Console.Write(result);
