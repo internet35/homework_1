9a7eeb73887ef2e4a149ff40291d9a0c42431957
BuildTools <unconfigured@null.spigotmc.org>
2022-12-01 19:24:49 +0300

    task_12_13

---
+++ homework/task_12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите  число");
+int n = Convert.ToInt32(Console.ReadLine()); 
+int sum=0;
+
+while(n>0)
+{
+    sum=sum+n%10;
+    n=n/10;
+}
+  
+Console.WriteLine(sum); 
---
+++ homework/task_13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.Write("n= ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i;
+int[] array= new int[8];
+for (i = 0; i < n; i ++)
+{
+    Console.Write ("array[{0}]=",i);
+    array[i]=Convert.ToInt32(Console.ReadLine());
+
+}
+
+Console.WriteLine($"[{string.Join(",", array)}]");
