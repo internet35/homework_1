4d267487b8e24e4078b035add493034538ea6c4d
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 15:59:56 +0300

    task_14

---
+++ homework/task_14/Program.cs
@@ -0,0 +1,24 @@
+﻿void inputArray(int[] array)
+{
+for (int i=0; i<array.Length; i++)
+  array[i]=new Random().Next(100,1000);
+}
+void ReleaseArray(int[]array)
+{
+    int sumEven=0;
+    for (int i=0; i<array.Length;i++)
+    {
+        if(array[i]%2==0)
+        sumEven=sumEven+1;
+       
+    }
+ Console.WriteLine($"количество четных чисел  в массиве {sumEven}");
+
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива");
+int n=Convert.ToInt32(Console.ReadLine()); 
+int[] array= new int[n];
+inputArray(array);
+Console.WriteLine($"[{string.Join(",",array)}]");
+ReleaseArray(array);
