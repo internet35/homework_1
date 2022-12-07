bcfc7602dbd42bb71c7bdc1b0887f6e29fa3fd60
BuildTools <unconfigured@null.spigotmc.org>
2022-12-07 17:13:55 +0300

    task_16

---
+++ homework/task_16/Program.cs
@@ -0,0 +1,27 @@
+﻿void inputArray(double[] array)
+{
+for (int i=0; i<array.Length; i++)
+  array[i]=new Random().Next(0,100);
+}
+void ReleaseArray(double[]array)
+{
+    double min=array[0];
+    double max=array[0];
+    for (int i=0; i<array.Length;i++)
+    {
+        if(array[i]<min)
+        min=array[i];
+        else if (array[i]>max)
+        max=array[i];
+       
+    }
+ Console.WriteLine($"разница между максимальным и минимальным элементов массива {max-min}");
+
+}
+Console.Clear();
+Console.Write("Введите кол-во элементов массива");
+int n=Convert.ToInt32(Console.ReadLine()); 
+double[] array= new double[n];
+inputArray(array);
+Console.WriteLine($"[{string.Join(",",array)}]");
+ReleaseArray(array);
