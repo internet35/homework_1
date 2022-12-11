6ce387fe605f69feb028ec815579d7488f17119a
BuildTools <unconfigured@null.spigotmc.org>
2022-12-11 11:42:05 +0300

    task_17

---
+++ homework/task_17/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+Console.Write("n= ");
+int n = Convert.ToInt32(Console.ReadLine());
+int i;
+int result=0;
+int[] array= new int[n];
+for (i = 0; i < n; i ++)
+{
+    Console.Write ("array[{0}]=",i);
+    array[i]=Convert.ToInt32(Console.ReadLine());
+    if (array[i]>0)
+{
+    result=result+1;
+}
+}
+
+Console.WriteLine($"[{string.Join(",", array)}]");
+Console.WriteLine(result);
