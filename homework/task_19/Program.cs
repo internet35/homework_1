ab813c07f5f792b502d011580e4d003204f30497
BuildTools <unconfigured@null.spigotmc.org>
2022-12-14 17:24:15 +0300

    task_19

---
+++ homework/task_19/Program.cs
@@ -0,0 +1,21 @@
+﻿void InputMatrix(double[, ] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(new Random().NextDouble()*(20-10)+10,2);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+ string [] coord = Console.ReadLine().Split(" ");
+int row = int.Parse(coord[0]);
+int col = int.Parse(coord[1]);
+double[,] matrix = new double[row, col];
+InputMatrix(matrix);
