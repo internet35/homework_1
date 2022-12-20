2a249fd79c8ba9af729310edff355921fdb4104f
BuildTools <unconfigured@null.spigotmc.org>
2022-12-20 17:09:56 +0300

    task_22

---
+++ homework/task_22/Program.cs
@@ -0,0 +1,13 @@
+﻿int sum(int m, int n)
+{
+    if (n==m)
+    return m;
+    return sum(m,n-1)+n;
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(sum(m,n));
